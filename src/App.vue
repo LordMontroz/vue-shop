0694ff42612aafdfa74e179eec028b3706a1df93
Rasul Gasbanov <gasbanovrasul@gmail.com>
2025-11-19 01:03:02 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup></script>
+
+<template>
+  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+    <header class="flex justify-between border-b border-slate-300 px-8">
+      <div class="flex items-center">
+        <img src="/logo.svg" alt="logo" class="w-10" />
+        <div>
+          <h2 class="text-xl font-bold uppercase">05.ru</h2>
+          <p class="text-slate-500">Интернет-магазин</p>
+        </div>
+      </div>
+
+      <ul>
+        <li>
+          <img src="/cart2.svg" alt="cart" />
+          <b>1205 руб.</b>
+        </li>
+        <li>
+          <img src="/heart.svg" alt="cart" />
+          <b>Избранное</b>
+        </li>
+      </ul>
+    </header>
+
+    <h1>Rasul Shop</h1>
+  </div>
+</template>
